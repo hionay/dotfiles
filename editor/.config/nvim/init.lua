369738a5d485bfd1c77861e0abbadbdf1dd5f131
Halil ibrahim Onay <hionay@gmail.com>
2026-04-08 10:56:12 +0300

    chore: update minimax configs

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -115,8 +115,8 @@ Config.new_autocmd = function(event, pattern, callback, desc)
   vim.api.nvim_create_autocmd(event, opts)
 end
 
--- Define custom `vim.pack.add()` hook helper. See `:h vim.pack-events`.
--- Example usage: see 'plugin/40_plugins.lua'.
+-- Define custom `vim.pack.add()` hook helper. Plugin data is passed as
+-- argument to the callback. See `:h vim.pack-events`.
 Config.on_packchanged = function(plugin_name, kinds, callback, desc)
   local f = function(ev)
     local name, kind = ev.data.spec.name, ev.data.kind
@@ -126,7 +126,7 @@ Config.on_packchanged = function(plugin_name, kinds, callback, desc)
     if not ev.data.active then
       vim.cmd.packadd(plugin_name)
     end
-    callback()
+    callback(ev.data)
   end
   Config.new_autocmd("PackChanged", "*", f, desc)
 end
