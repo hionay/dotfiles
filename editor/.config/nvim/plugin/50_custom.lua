bb0bbedad79a98668d49da09807591404cb662f6
Halil ibrahim Onay <hionay@gmail.com>
2026-04-08 10:56:22 +0300

    chore add eslint to linters

--- editor/.config/nvim/plugin/50_custom.lua
+++ editor/.config/nvim/plugin/50_custom.lua
@@ -330,6 +330,8 @@ later(function()
 
   require("lint").linters_by_ft = {
     php = { "phpstan", "phpcs" },
+    typescript = { "eslint" },
+    javascript = { "eslint" },
   }
 
   vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
